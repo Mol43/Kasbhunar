62606c0cf3ef18cd25c469d1f63167abcb4ab8b1
Mexa <160151005+Mol43@users.noreply.github.com>
2024-12-28 18:42:13 +0500

    Update AloqaPage.jsx

--- src/pages/AloqaPage/AloqaPage.jsx
+++ src/pages/AloqaPage/AloqaPage.jsx
@@ -1,4 +1,4 @@
-import Footer from "../../components/footer/Footer"
+import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
 import Aloqa from "../../components/Aloqa/Aloqa"
 
