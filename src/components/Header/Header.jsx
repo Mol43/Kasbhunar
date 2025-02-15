a686ac01ddadc7eef35cef7c9da6a69967342f55
mexa-bro <dantdm0106@gmail.com>
2025-02-15 20:29:58 +0500

    chek merj

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,11 +10,7 @@ function Header() {
 
     const obj = [
         {
-<<<<<<< HEAD
             img: '/img/3ed65c588dab63b697649ceb5366772f 1.png',
-=======
-            img: '/img/avtomobilsozlik.png',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             heading: 'Avtomobilsozlik',
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
@@ -22,65 +18,40 @@ function Header() {
             date: '11.10.2024',
         },
         {
-<<<<<<< HEAD
             img: '/img/kasb-tikuvchilik.png',
             heading: 'Tikuvchilik',
-=======
-            img: '/img/avtomobilsozlik.png',
-            heading: 'Avtomobilsozlik',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
             coments: 0,
             date: '11.10.2024',
         },
         {
-<<<<<<< HEAD
             img: '/img/agrobiznes.png',
             heading: 'Agrobiznes, marketing',
-=======
-            img: '/img/avtomobilsozlik.png',
-            heading: 'Avtomobilsozlik',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
             coments: 0,
             date: '11.10.2024',
         },
         {
-<<<<<<< HEAD
             img: '/img/mashinist.png',
             heading: 'Traktor mashinist',
-=======
-            img: '/img/avtomobilsozlik.png',
-            heading: 'Avtomobilsozlik',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
             coments: 0,
             date: '11.10.2024',
         },
         {
-<<<<<<< HEAD
             img: '/img/axborot.png',
             heading: 'Axborot texalogiya',
-=======
-            img: '/img/avtomobilsozlik.png',
-            heading: 'Avtomobilsozlik',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
             coments: 0,
             date: '11.10.2024',
         },
         {
-<<<<<<< HEAD
             img: '/img/dvigatel.png',
             heading: 'Dvigatellarni ta’mirlash',
-=======
-            img: '/img/avtomobilsozlik.png',
-            heading: 'Avtomobilsozlik',
->>>>>>> 837689af46bcfe257758ffbf6a3036b59d20f9a3
             paragraph: 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
             prosmotr: 77,
             coments: 0,
